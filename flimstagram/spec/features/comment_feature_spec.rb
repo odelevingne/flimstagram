9b0ad1e1c507c373005f96962573a3d0a280d58e
Oliver Delevingne <oliver@delevingne.co.uk>
2014-05-28 11:37:38 +0100

    created first feature test for comments

--- flimstagram/spec/features/comment_feature_spec.rb
+++ flimstagram/spec/features/comment_feature_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe 'writing comments' do 
+	before do 
+			user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
+			login_as user
+			Post.create(description: 'Comment test')
+		end
 
-	
+	specify 'a post has no comments when first uploaded' do
+		visit '/posts'
+
+		expect(page).to have_content '0 comments'
+	end
 end
