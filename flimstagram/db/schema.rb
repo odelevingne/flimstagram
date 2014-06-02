5b16a68409c65db060af24eda285177d1f26f2e0
Oliver Delevingne <oliver@delevingne.co.uk>
2014-06-02 19:00:19 +0100

    added user_id to likes model with an aim to implement ability to unlike a post

--- flimstagram/db/schema.rb
+++ flimstagram/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602153700) do
+ActiveRecord::Schema.define(version: 20140602175847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,11 @@ ActiveRecord::Schema.define(version: 20140602153700) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id_id"
   end
 
   add_index "likes", ["post_id"], name: "index_likes_on_post_id", using: :btree
+  add_index "likes", ["user_id_id"], name: "index_likes_on_user_id_id", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "description"
--- flimstagram/spec/features/like_feature_spec.rb
+++ flimstagram/spec/features/like_feature_spec.rb
@@ -2,63 +2,87 @@ require 'spec_helper'
 
 describe 'Liking a post:' do
 
-	let(:user2) { User.create(email: 'scott@scott.com', password: '12345678', password_confirmation: '12345678') }
+	context 'Logged in:' do
+
+		let(:user2) { User.create(email: 'scott@scott.com', password: '12345678', password_confirmation: '12345678') }
 	
-	before do 
-		user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
-		login_as user
-		Post.create(description: 'Test', user: user, tag_names: 'test, tags')
+			before do 
+				user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
+				login_as user
+				Post.create(description: 'Test', user: user, tag_names: 'test, tags')
+			end
+
+			specify 'a post has no likes when first created' do
+				visit '/posts'
+
+				expect(page).to have_content '❤ 0'
+			end
+
+			specify 'when a user clicks the like button it registers a like' do
+				visit '/posts'
+
+				click_link '❤ 0'
+				expect(page).to have_content '❤ 1'
+
+			end
+
+			specify 'a user can undo a like' do
+				
+				visit '/posts'
+
+				click_link '❤ 0'
+				click_link '❤ 1'
+
+				expect(page).to have_content('❤ 0')
+			end
+
+			specify "a user can see another user's like" do
+
+				visit '/posts'
+
+				click_link '❤ 0'
+
+				logout(:user)
+
+				login_as user2
+
+				expect(page).to have_content '❤ 1'
+			end
+
+			specify 'a user can like a post that has been liked by another user' do
+				visit '/posts'
+
+				click_link '❤ 0'
+
+				logout(:user)
+
+				login_as user2
+
+				click_link '❤ 1'
+
+				expect(page).to have_content '❤ 2'
+			end
 	end
 
-	specify 'a post has no likes when first created' do
-		visit '/posts'
+	context 'Logged out:' do
 
-		expect(page).to have_content '❤ 0'
+		before do 
+			user = User.create(email: 'ollie@ollie.com', password: '12345678', password_confirmation: '12345678')
+			login_as user
+			Post.create(description: 'Test', user: user, tag_names: 'test, tags')
+		end
+
+		xspecify 'you can only like a post if logged in' do
+
+			visit '/posts'
+
+			click_link '❤ 0'
+
+			expect(page).to have_link '❤ 1'
+
+			logout(:user)
+
+			expect(page).not_to have_link '❤ 1'
+		end
 	end
-
-	specify 'when a user clicks the like button it registers a like' do
-		visit '/posts'
-
-		click_link '❤ 0'
-		expect(page).to have_content '❤ 1'
-
-	end
-
-	specify 'a user can undo a like' do
-		
-		visit '/posts'
-
-		click_link '❤ 0'
-		click_link '❤ 1'
-
-		expect(page).to have_content('❤ 0')
-	end
-
-	specify "a user can see another user's like" do
-
-		visit '/posts'
-
-		click_link '❤ 0'
-
-		logout(:user)
-
-		login_as user2
-
-		expect(page).to have_content '❤ 1'
-	end
-
-	specify 'a user can like a post that has been liked by another user' do
-		visit '/posts'
-
-		click_link '❤ 0'
-
-		logout(:user)
-
-		login_as user2
-
-		click_link '❤ 1'
-
-		expect(page).to have_content '❤ 2'
-	end
-
 end
